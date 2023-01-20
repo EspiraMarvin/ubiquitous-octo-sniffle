a6a261f1e54b57f2b558f2506b1f682d65742509
marvin espira <espiramarvin@gmail.com>
2023-01-20 20:08:28 +0300

    fix:code refactoring and cleanup on users' tests

--- src/components/users/__test__/UserListItem.test.tsx
+++ src/components/users/__test__/UserListItem.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import UserListItem from '../UserListItem'
 
 import { BrowserRouter } from 'react-router-dom'
--- src/components/users/__test__/UsersList.test.tsx
+++ src/components/users/__test__/UsersList.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import UsersList from '../UsersList'
 import { BrowserRouter } from 'react-router-dom'
 
