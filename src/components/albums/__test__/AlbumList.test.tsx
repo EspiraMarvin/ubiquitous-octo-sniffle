147ebda7e83fb942cd943c78f16f3e39031b2fa0
marvin espira <espiramarvin@gmail.com>
2023-01-20 17:10:42 +0300

    feat: adds AlbumList tests

---
+++ src/components/albums/__test__/AlbumList.test.tsx
@@ -0,0 +1,44 @@
+import { render, screen, fireEvent, waitFor } from '@testing-library/react'
+import AlbumList from '../AlbumList'
+import { BrowserRouter } from 'react-router-dom'
+
+const MockAlbumList = () => {
+  return (
+    <BrowserRouter>
+      <AlbumList
+        user={{
+          name: 'Ervin Howel',
+          albumCount: '10',
+          id: '1',
+          username: 'Ervin',
+          email: 'Ervin@gmail.com',
+          address: {
+            street: 'nai',
+            suite: 'sur3',
+            city: 'mbs',
+            zipcode: '234sd',
+          },
+          phone: '2343525345',
+          website: 'facebook.com',
+          geo: { lat: '234232', lng: '93234' },
+          company: {
+            name: 'company 1',
+            catchPhrase: 'kompany 1',
+            bs: 'bss',
+          },
+        }}
+      />
+    </BrowserRouter>
+  )
+}
+
+describe('<AlbumItem />', () => {
+  beforeEach(async () => {
+    render(<MockAlbumList />)
+  })
+
+  it('should have Albums List title', async () => {
+    const AlbumDetailsTitle = screen.getByText(/Albums List/i)
+    expect(AlbumDetailsTitle).toBeInTheDocument()
+  })
+})
