9f98e82c9802b4a50866e76e67912ec548df41e9
marvin espira <espiramarvin@gmail.com>
2023-01-20 17:05:35 +0300

    feat: adds Photo Page tests

---
+++ src/pages/__test__/PhotoPage.test.tsx
@@ -0,0 +1,25 @@
+import { render, screen, waitFor } from '@testing-library/react'
+import { BrowserRouter } from 'react-router-dom'
+import PhotoPage from '../PhotoPage'
+
+const MockPhotoPage = () => {
+  return (
+    <BrowserRouter>
+      <PhotoPage />
+    </BrowserRouter>
+  )
+}
+describe('Photo Page', () => {
+  beforeEach(async () => {
+    render(<MockPhotoPage />)
+  })
+
+  it('should have the correct document title', async () => {
+    await waitFor(() => expect(document.title).toEqual('Album Photo Details'))
+  })
+
+  it('should render correct page title', async () => {
+    const PhotoDetailsTitle = screen.getByText(/Album Photo Details/i)
+    expect(PhotoDetailsTitle).toBeInTheDocument()
+  })
+})
