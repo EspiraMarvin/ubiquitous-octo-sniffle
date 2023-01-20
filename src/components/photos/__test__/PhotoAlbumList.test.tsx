97ac92866d03de93502d68987c14f1ab4588fb3a
marvin espira <espiramarvin@gmail.com>
2023-01-20 17:13:34 +0300

    feat: adds PhotoAlbumList tests to ensure it renders correctly

---
+++ src/components/photos/__test__/PhotoAlbumList.test.tsx
@@ -0,0 +1,41 @@
+import { render, screen, fireEvent, waitFor } from '@testing-library/react'
+import PhotoAlbumList from '../PhotoAlbumList'
+import { BrowserRouter } from 'react-router-dom'
+
+const MockAPhotoAlbumList = () => {
+  return (
+    <BrowserRouter>
+      <PhotoAlbumList
+        albumPhotos={[
+          {
+            albumId: 1,
+            id: 1,
+            title: 'accusamus beatae ad facilis cum similique qui sunt',
+            url: 'https://via.placeholder.com/600/92c952',
+            thumbnailUrl: 'https://via.placeholder.com/150/92c952',
+          },
+          {
+            albumId: 2,
+            id: 2,
+            title: 'reprehenderit est deserunt velit ipsam',
+            url: 'https://via.placeholder.com/600/771796',
+            thumbnailUrl: 'https://via.placeholder.com/150/771796',
+          },
+        ]}
+        userId={1}
+        albumId={1}
+      />
+    </BrowserRouter>
+  )
+}
+
+describe('<PhotoAlbumList />', () => {
+  beforeEach(async () => {
+    render(<MockAPhotoAlbumList />)
+  })
+
+  it('should render multiple photo albums in a list', async () => {
+    const photoAlbumElements = await screen.findAllByTestId(/photo-album/i)
+    expect(photoAlbumElements.length).toBe(2)
+  })
+})
