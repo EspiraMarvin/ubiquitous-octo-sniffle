3801a38c56515db517fa2aebe26477035ff6e725
marvin espira <espiramarvin@gmail.com>
2023-01-20 20:09:09 +0300

    fix:code refactoring and cleanup App test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,5 @@
 import { render, screen } from '@testing-library/react'
-import { BrowserRouter, MemoryRouter } from 'react-router-dom'
-import userEvent from '@testing-library/user-event'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 
 const MockApp = () => {
