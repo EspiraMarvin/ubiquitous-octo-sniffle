36349a51704a6bcd855c44e960d45cca54729f26
marvin espira <espiramarvin@gmail.com>
2023-01-20 16:46:08 +0300

    fix: code clean up and adds sentry for logging

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,50 @@
-import React from 'react'
-import logo from './logo.svg'
+import { Routes, Route, useNavigate } from 'react-router-dom'
+import { useEffect, useState } from 'react'
 import './App.css'
+import NavBar from './components/ui/NavBar'
+import HomePage from './pages/HomePage'
+import UsersPage from './pages/UsersPage'
+import UserPage from './pages/UserPage'
+import AlbumPage from './pages/AlbumPage'
+import PhotoPage from './pages/PhotoPage'
+import NotFoundPage from './pages/NotFoundPage'
+import * as Sentry from '@sentry/react'
 
 function App() {
+  const navigate = useNavigate()
+  const [token, setToken] = useState<string | null>(null)
+  useEffect(() => {
+    let authToken = sessionStorage.getItem('AuthToken')
+    setToken(authToken)
+    if (!authToken) {
+      navigate('/')
+    }
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="h-screen">
+      <header>
+        <NavBar />
       </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+
+          <Route path="/users" element={<UsersPage />} />
+          <Route path="/users/:userId" element={<UserPage />} />
+          <Route
+            path="/users/:userId/albums/:albumId"
+            element={<AlbumPage />}
+          />
+          <Route
+            path="/users/:userId/albums/:albumId/photos/:photoId"
+            element={<PhotoPage />}
+          />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </main>
     </div>
   )
 }
 
-export default App
+export default Sentry.withProfiler(App)
