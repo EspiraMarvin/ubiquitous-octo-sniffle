64a94ed9650fe68c0c83852df91b5e872ac79025
marvin espira <espiramarvin@gmail.com>
2023-01-21 22:02:05 +0300

    fix: styling fixes for all screen responsiveness

--- src/components/albums/AlbumDetails.tsx
+++ src/components/albums/AlbumDetails.tsx
@@ -7,15 +7,13 @@ interface AlbumDetailsProps {
 function AlbumDetails({ albumInfo }: AlbumDetailsProps) {
   if (!albumInfo)
     return (
-      <div className="flex h-screen justify-center items-center">
+      <div className="flex justify-center items-center">
         <Loader color="blue" />
       </div>
     )
   return (
-    <div>
-      <div className="text-center py-4">
-        Title: <span className="font-bold capitalize">{albumInfo.title}</span>
-      </div>
+    <div className="text-center py-3">
+      Title: <span className="font-bold capitalize">{albumInfo.title}</span>
     </div>
   )
 }
